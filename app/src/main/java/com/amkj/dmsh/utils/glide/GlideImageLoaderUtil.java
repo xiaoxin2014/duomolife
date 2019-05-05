7c85b486d9763f79c3e0e72e58be3abb28dedb37
shizhiqing <184689929@qq.com>
2019-05-05 10:23:27 +0800

    v4.0.0 release完整版本

--- app/src/main/java/com/amkj/dmsh/utils/glide/GlideImageLoaderUtil.java
+++ app/src/main/java/com/amkj/dmsh/utils/glide/GlideImageLoaderUtil.java
@@ -5,6 +5,8 @@ import android.content.Context;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.graphics.drawable.Drawable;
+import android.opengl.GLES10;
+import android.os.Build;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.text.TextUtils;
@@ -30,6 +32,12 @@ import java.net.URL;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.concurrent.ExecutionException;
+
+import javax.microedition.khronos.egl.EGL10;
+import javax.microedition.khronos.egl.EGLConfig;
+import javax.microedition.khronos.egl.EGLContext;
+import javax.microedition.khronos.egl.EGLDisplay;
+import javax.microedition.khronos.egl.EGLSurface;
 
 import io.reactivex.Observable;
 import io.reactivex.ObservableEmitter;
@@ -482,9 +490,11 @@ public class GlideImageLoaderUtil {
                             }
                         }
                         if (isContextExisted(context) && imageViewX != null) {
-                            Glide.with(context).asDrawable().load(imgUrlX)
-                                    .apply(new RequestOptions().dontAnimate()
+                            Glide.with(context).load(imgUrlX)
+                                    .apply(new RequestOptions()
                                             .error(R.drawable.load_loading_image)
+                                            .skipMemoryCache(true)
+                                            .diskCacheStrategy(DiskCacheStrategy.DATA)
                                             .override(imgWidth, imgHeight))
                                     .into(imageViewX);
                         }
@@ -512,6 +522,63 @@ public class GlideImageLoaderUtil {
                 }
             });
         }
+    }
+
+
+    public static int getOpenglRenderLimitValue() {
+        int maxsize;
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+            maxsize = getOpenglRenderLimitEqualAboveLollipop();
+        } else {
+            maxsize = getOpenglRenderLimitBelowLollipop();
+        }
+        return maxsize == 0 ? 4096 : maxsize;
+    }
+
+    private static int getOpenglRenderLimitBelowLollipop() {
+        int[] maxSize = new int[1];
+        GLES10.glGetIntegerv(GLES10.GL_MAX_TEXTURE_SIZE, maxSize, 0);
+        return maxSize[0];
+    }
+
+    private static int getOpenglRenderLimitEqualAboveLollipop() {
+        EGL10 egl = (EGL10) EGLContext.getEGL();
+        EGLDisplay dpy = egl.eglGetDisplay(EGL10.EGL_DEFAULT_DISPLAY);
+        int[] vers = new int[2];
+        egl.eglInitialize(dpy, vers);
+        int[] configAttr = {
+                EGL10.EGL_COLOR_BUFFER_TYPE, EGL10.EGL_RGB_BUFFER,
+                EGL10.EGL_LEVEL, 0,
+                EGL10.EGL_SURFACE_TYPE, EGL10.EGL_PBUFFER_BIT,
+                EGL10.EGL_NONE
+        };
+        EGLConfig[] configs = new EGLConfig[1];
+        int[] numConfig = new int[1];
+        egl.eglChooseConfig(dpy, configAttr, configs, 1, numConfig);
+        if (numConfig[0] == 0) {// TROUBLE! No config found.
+        }
+        EGLConfig config = configs[0];
+        int[] surfAttr = {
+                EGL10.EGL_WIDTH, 64,
+                EGL10.EGL_HEIGHT, 64,
+                EGL10.EGL_NONE
+        };
+        EGLSurface surf = egl.eglCreatePbufferSurface(dpy, config, surfAttr);
+        final int EGL_CONTEXT_CLIENT_VERSION = 0x3098;// missing in EGL10
+        int[] ctxAttrib = {
+                EGL_CONTEXT_CLIENT_VERSION, 1,
+                EGL10.EGL_NONE
+        };
+        EGLContext ctx = egl.eglCreateContext(dpy, config, EGL10.EGL_NO_CONTEXT, ctxAttrib);
+        egl.eglMakeCurrent(dpy, surf, surf, ctx);
+        int[] maxSize = new int[1];
+        GLES10.glGetIntegerv(GLES10.GL_MAX_TEXTURE_SIZE, maxSize, 0);
+        egl.eglMakeCurrent(dpy, EGL10.EGL_NO_SURFACE, EGL10.EGL_NO_SURFACE,
+                EGL10.EGL_NO_CONTEXT);
+        egl.eglDestroySurface(dpy, surf);
+        egl.eglDestroyContext(dpy, ctx);
+        egl.eglTerminate(dpy);
+        return maxSize[0];
     }
 
     /**
