205e7195226fa85f3f2d4c8425fd6d3a2586fb0e
shizhiqing <184689929@qq.com>
2019-05-13 17:19:05 +0800

    v4.1.0 福利社关联商品添加分页加载

--- app/src/main/java/com/amkj/dmsh/dominant/adapter/QualityOsMailHeaderAdapter.java
+++ app/src/main/java/com/amkj/dmsh/dominant/adapter/QualityOsMailHeaderAdapter.java
@@ -8,6 +8,7 @@ import android.view.View;
 import android.widget.ImageView;
 
 import com.amkj.dmsh.R;
+import com.amkj.dmsh.bean.DMLThemeEntity;
 import com.amkj.dmsh.bean.DMLThemeEntity.DMLThemeBean;
 import com.amkj.dmsh.bean.DMLThemeEntity.DMLThemeBean.DMLGoodsBean;
 import com.amkj.dmsh.constant.ConstantMethod;
@@ -15,13 +16,21 @@ import com.amkj.dmsh.constant.ConstantVariable;
 import com.amkj.dmsh.dominant.activity.DoMoLifeWelfareDetailsActivity;
 import com.amkj.dmsh.dominant.activity.QualityOverseasDetailsActivity;
 import com.amkj.dmsh.homepage.adapter.DuMoLifeHorRecyclerAdapter;
+import com.amkj.dmsh.network.NetLoadListenerHelper;
+import com.amkj.dmsh.network.NetLoadUtils;
 import com.amkj.dmsh.shopdetails.activity.ShopScrollDetailsActivity;
 import com.amkj.dmsh.utils.glide.GlideImageLoaderUtil;
+import com.amkj.dmsh.views.HorizontalLoadMoreView;
 import com.chad.library.adapter.base.BaseQuickAdapter;
 import com.chad.library.adapter.base.BaseViewHolder;
+import com.google.gson.Gson;
 
-import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
+
+import static com.amkj.dmsh.constant.ConstantVariable.ERROR_CODE;
+import static com.amkj.dmsh.constant.Url.H_DML_THEME;
 
 /**
  * @author LGuiPeng
@@ -33,22 +42,20 @@ import java.util.List;
 public class QualityOsMailHeaderAdapter extends BaseQuickAdapter<DMLThemeBean, BaseViewHolder> {
     private final Activity context;
     private final String type;
-    private List<DMLGoodsBean> goodsList;
+    private int page = 1;
+    private final List<DMLThemeBean> mDMLThemeBeanList;
 
     public QualityOsMailHeaderAdapter(Activity context, List<DMLThemeBean> dmlThemeBeanList, String type) {
         super(R.layout.adapter_welfare_header_item, dmlThemeBeanList);
         this.type = type;
         this.context = context;
+        mDMLThemeBeanList = dmlThemeBeanList;
     }
 
     @Override
     protected void convert(BaseViewHolder helper, final DMLThemeBean dMLThemeBean) {
         ImageView iv_cover_bg_header_img = helper.getView(R.id.iv_cover_bg_header_img);
         GlideImageLoaderUtil.loadCenterCrop(context, iv_cover_bg_header_img, dMLThemeBean.getPicUrl());
-        goodsList = new ArrayList<>();
-        if (dMLThemeBean.getGoods() != null && dMLThemeBean.getGoods().size() > 0) {
-            goodsList.addAll(dMLThemeBean.getGoods());
-        }
         iv_cover_bg_header_img.setTag(R.id.iv_tag, dMLThemeBean);
         iv_cover_bg_header_img.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -70,41 +77,91 @@ public class QualityOsMailHeaderAdapter extends BaseQuickAdapter<DMLThemeBean, B
         });
         RecyclerView rv_wrap_bar_none = helper.getView(R.id.rv_wrap_bar_none);
         rv_wrap_bar_none.setLayoutManager(new LinearLayoutManager(context, LinearLayoutManager.HORIZONTAL, false));
-        DuMoLifeHorRecyclerAdapter horGvAdapter = new DuMoLifeHorRecyclerAdapter(context, goodsList);
+        DuMoLifeHorRecyclerAdapter horGvAdapter = new DuMoLifeHorRecyclerAdapter(context, dMLThemeBean.getGoods());
+        horGvAdapter.setLoadMoreView(new HorizontalLoadMoreView());
         rv_wrap_bar_none.setAdapter(horGvAdapter);
-        horGvAdapter.setNewData(goodsList);
-        horGvAdapter.setOnItemClickListener(new OnItemClickListener() {
-            @Override
-            public void onItemClick(BaseQuickAdapter adapter, View view, int position) {
-                DMLGoodsBean dMLGoodsBean = (DMLGoodsBean) view.getTag();
-                if (dMLGoodsBean != null) {
-                    //跳转良品详情页
-                    Intent intent = new Intent();
-                    if (dMLGoodsBean.getItemType() == ConstantVariable.TYPE_0) {
-                        intent.setClass(context, ShopScrollDetailsActivity.class);
-                        intent.putExtra("productId", String.valueOf(dMLGoodsBean.getId()));
-                        intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
-                        //记录埋点参数sourceId(福利社专题对应的ID)
-                        ConstantMethod.saveSourceId(context.getClass().getSimpleName(), String.valueOf(dMLThemeBean.getId()));
-                        context.startActivity(intent);
-                    } else if (dMLGoodsBean.getItemType() == ConstantVariable.TYPE_1) {
-                        switch (type) {
-                            case "welfare":
-                                intent.setClass(context, DoMoLifeWelfareDetailsActivity.class);
-                                intent.putExtra("welfareId", String.valueOf(dMLGoodsBean.getId()));
-                                intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
-                                context.startActivity(intent);
-                                break;
-                            case "overseas":
-                                intent.setClass(context, QualityOverseasDetailsActivity.class);
-                                intent.putExtra("overseasId", String.valueOf(dMLGoodsBean.getId()));
-                                intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
-                                context.startActivity(intent);
-                                break;
-                        }
+        horGvAdapter.setOnItemClickListener((adapter, view, position) -> {
+            DMLGoodsBean dMLGoodsBean = (DMLGoodsBean) view.getTag();
+            if (dMLGoodsBean != null) {
+                //跳转良品详情页
+                Intent intent = new Intent();
+                if (dMLGoodsBean.getItemType() == ConstantVariable.TYPE_0) {
+                    intent.setClass(context, ShopScrollDetailsActivity.class);
+                    intent.putExtra("productId", String.valueOf(dMLGoodsBean.getId()));
+                    intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+                    //记录埋点参数sourceId(福利社专题对应的ID)
+                    ConstantMethod.saveSourceId(context.getClass().getSimpleName(), String.valueOf(dMLThemeBean.getId()));
+                    context.startActivity(intent);
+                } else if (dMLGoodsBean.getItemType() == ConstantVariable.TYPE_1) {
+                    switch (type) {
+                        case "welfare":
+                            intent.setClass(context, DoMoLifeWelfareDetailsActivity.class);
+                            intent.putExtra("welfareId", String.valueOf(dMLGoodsBean.getId()));
+                            intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+                            context.startActivity(intent);
+                            break;
+                        case "overseas":
+                            intent.setClass(context, QualityOverseasDetailsActivity.class);
+                            intent.putExtra("overseasId", String.valueOf(dMLGoodsBean.getId()));
+                            intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+                            context.startActivity(intent);
+                            break;
                     }
                 }
             }
         });
+        horGvAdapter.setOnLoadMoreListener(() -> {
+            page++;
+            getWelfareThemeData(horGvAdapter, helper.getAdapterPosition());
+        }, rv_wrap_bar_none);
+    }
+
+    //    福利社主题商品列表
+    private void getWelfareThemeData(DuMoLifeHorRecyclerAdapter horGvAdapter, int position) {
+        Map<String, Object> params = new HashMap<>();
+        params.put("currentPage", 1);
+        params.put("showCount", 1);
+        params.put("goodsCurrentPage", page);
+        params.put("goodsShowCount", ConstantVariable.TOTAL_COUNT_TEN);
+        NetLoadUtils.getNetInstance().loadNetDataPost(context, H_DML_THEME
+                , params, new NetLoadListenerHelper() {
+                    @Override
+                    public void onSuccess(String result) {
+                        Gson gson = new Gson();
+                        DMLThemeEntity dmlTheme = gson.fromJson(result, DMLThemeEntity.class);
+                        if (dmlTheme != null) {
+                            List<DMLThemeBean> themeList = dmlTheme.getThemeList();
+                            if (themeList != null && themeList.size() > 0) {
+                                DMLThemeBean dmlThemeBean = themeList.get(0);
+                                if (dmlThemeBean != null) {
+                                    List<DMLGoodsBean> goods = dmlThemeBean.getGoods();
+                                    if (goods != null && goods.size() > 0) {
+                                        mDMLThemeBeanList.get(position).getGoods().addAll(goods);
+                                        horGvAdapter.notifyItemChanged(position);
+                                        horGvAdapter.loadMoreComplete();
+                                    } else if (ERROR_CODE.equals(dmlTheme.getCode())) {
+                                        ConstantMethod.showToast(dmlTheme.getMsg());
+                                        horGvAdapter.loadMoreFail();
+                                    } else {
+                                        horGvAdapter.loadMoreEnd();
+                                    }
+                                }
+                            } else {
+                                horGvAdapter.loadMoreEnd();
+                            }
+                        } else {
+                            horGvAdapter.loadMoreEnd();
+                        }
+                    }
+
+                    @Override
+                    public void onNotNetOrException() {
+                        horGvAdapter.loadMoreFail();
+                    }
+                });
+    }
+
+    public void setPage(int page) {
+        this.page = page;
     }
 }
